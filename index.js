65a0b8d6d8a45e59a271715e0fc3252bad88c799
phantomcoder11 <dhritiraj_ug@ece.nits.ac.in>
2022-12-28 13:21:58 +0530

    index.js modified

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Inotebook App listening on port ${port}`)
 })

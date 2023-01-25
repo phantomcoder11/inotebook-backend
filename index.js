fe0286309e4e3dea2cca9b00dc364df0c48fd4cb
phantomcoder11 <dhritiraj_ug@ece.nits.ac.in>
2023-01-25 20:27:44 +0530

    index.js modifies

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 require('./db')
 //connectToMongo();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
